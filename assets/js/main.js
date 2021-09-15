614a073041d285242c14ff5fad9c8bab3af087c7
AntonyOnScript <santhiagoafk@gmail.com>
2021-09-15 14:26:48 -0300

    Menu has been improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
 const NAV_MENU = document.querySelector('.nav.main-nav')
+const SEARCH_BAR = document.querySelector('.search-bar')
+const BTN_LINK = document.querySelector('.border-button-link')
+const LINKS_MENU = document.querySelectorAll('.nav-link')
 
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 2){
         NAV_MENU.style.backgroundColor = '#fff'
+        SEARCH_BAR.style.visibility = 'visible'
+        BTN_LINK.style.color = 'var(--color-one)'
+        mudaCorLinksMenu()
     } else {
         NAV_MENU.style.backgroundColor = 'transparent'
+        SEARCH_BAR.style.visibility = 'hidden'
+        BTN_LINK.style.color = 'white'
+        mudaCorLinksMenu(1)
+    }
+}
+
+function mudaCorLinksMenu(option) {
+    if(option === 1) {
+        LINKS_MENU.forEach(element => {
+            element.style.color = 'white'
+        })
+        
+    } else {
+        LINKS_MENU.forEach(element => {
+            element.style.color = 'var(--color-one)'
+        })
     }
 }
