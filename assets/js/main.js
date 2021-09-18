ddf8d50655c3242e115c2efec2e6862ea7e444ce
AntonyOnScript <santhiagoafk@gmail.com>
2021-09-18 19:50:09 -0300

    Menu got finalized

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ const NAV_MENU = document.querySelector('.nav.main-nav')
 const SEARCH_BAR = document.querySelector('.search-bar')
 const BTN_LINK = document.querySelector('.border-button-link')
 const LINKS_MENU = document.querySelectorAll('.nav-link')
+const ALL_MENU_LINKS = document.querySelector(".links-menu")
+const MOBILE_MENU_CONTAINER = document.querySelector('.mobile-menu .uk-offcanvas-bar')
+const MOBILE_MENU = document.querySelector('.mobile-menu  .uk-nav')
+const MOBILE_MENU_BUTTON = document.querySelector('.mobile-menu-button')
 
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 2){
@@ -28,4 +32,21 @@ function mudaCorLinksMenu(option) {
             element.style.color = 'var(--color-one)'
         })
     }
+}
+
+function loadMenuLinksIntoMobileMenu() {
+    const LINKS = ALL_MENU_LINKS.querySelectorAll('.nav-link')
+    LINKS.forEach(element => {
+        const li = document.createElement("li")
+        element = element.cloneNode(true)
+        element.removeAttribute('class')
+        li.appendChild(element)
+        MOBILE_MENU.appendChild(li)
+    })
+}
+
+loadMenuLinksIntoMobileMenu()
+
+MOBILE_MENU_BUTTON.onclick = () => {
+    MOBILE_MENU_CONTAINER.style.animation = 'showMenu .5s'
 }
