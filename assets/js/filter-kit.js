4dc796bfd93c7a589dedda3a0a6576961112dad5
AntonyOnScript <santhiagoafk@gmail.com>
2021-09-20 12:19:59 -0300

    Filter kit was prototyped

---
+++ assets/js/filter-kit.js
@@ -0,0 +1,41 @@
+const FILTER = document.querySelectorAll('.filter-kit')
+
+FILTER.forEach(element => {
+
+    const FILTER_BUTTON = element.querySelector('.filter-category-kit')
+
+    FILTER_BUTTON.onclick = () => {
+
+        const OPTIONS_CATEGORY_KIT = element.querySelector('.options-category-kit')
+        const FILTER_KIT_ICON = element.querySelector('.filter-kit span[uk-icon]')
+
+        if(OPTIONS_CATEGORY_KIT.style.display === 'flex') {
+
+            OPTIONS_CATEGORY_KIT.style.display = 'none'
+            FILTER_KIT_ICON.style.transition = 'all .3s'
+            FILTER_KIT_ICON.style.transform = 'rotate(0deg)'
+
+        } else {
+
+            const ALL_OTHER_CATEGORY_BLOCK = document.querySelectorAll('.options-category-kit')
+            ALL_OTHER_CATEGORY_BLOCK.forEach(element => {
+                element.style.display = 'none'
+            })
+
+            const ALL_OTHER_KIT_ICONS = document.querySelectorAll('.filter-kit span[uk-icon]')
+
+            ALL_OTHER_KIT_ICONS.forEach(element => {
+                element.style.transition = 'all .3s'
+                element.style.transform = 'rotate(0deg)'
+            })
+
+            OPTIONS_CATEGORY_KIT.style.display = 'flex'
+            FILTER_KIT_ICON.style.transition = 'all .3s'
+            FILTER_KIT_ICON.style.transform = 'rotate(-180deg)'
+
+        }
+
+    }
+
+})
+
